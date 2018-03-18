007e0ed2ae7ec570c916ea9dcef9db319558b17a
Chadia <chabib@masonlive.gmu.edu>
2018-03-18 11:26:54 -0400

    fixes in NewUser and comments for todo list
    Change-Id: I91b3083de56b51e3faff2717a27247f2e4b33130

--- dama/src/NewUser.java
+++ dama/src/NewUser.java
@@ -38,22 +38,46 @@ public class NewUser extends HttpServlet {
         String username = request.getParameter("username");
         String password1 = request.getParameter("password1");
         String password2 = request.getParameter("password2");
-        if (password1 == password2) {
+        if (password1.equals(password2)) {
+//TODO list added by Chadia
+// should present user-friendly message when password1 and password2 are not equal
+// should present user-friendly message when username already exist in DB (should not wait for SQL exception)
+// should add logging entry for user created
+// should remove hard-coded DB credentials from this file and from Validate.java. Instead read these creds from a server config file.
+// should add input validation (whitelist w/ regular expressions) for the email
+// should not be storing passwords in clear form. Instead store only salted hash.
+// should encrypt connections to the DB (use TLS encryption)
+// question: should one email be allowed to register multiple usernames?
+// consider: validating the exitance of the email (ie. send confirmation link to prove ownership of the email being registered)
+// consider enforcing a password length and complexity for the user's password
+// consider adding captcha for preventing bots from abusing this form (least important task for now)
 	        try {
-		        String email = request.getParameter("email");
+
+/*		        String email = request.getParameter("email");
 		        String username = request.getParameter("username");
 		        String password1 = request.getParameter("password1");
 		        String password2 = request.getParameter("password2");
-		
-		        	Class.forName("oracle.jdbc.driver.OracleDriver");
+*/		
+
+
+/*		        	Class.forName("oracle.jdbc.driver.OracleDriver");
 		    		Connection con = DriverManager.getConnection ("jdbc:oracle:thin:@dama.cc0rojk8d4jm.us-east-1.rds.amazonaws.com:1521:dama","swe681","SWEpass123");
 		    		String query = "INSERT INTO USERS VALUES (?,?,?)";
+*/
+                                Class.forName("org.postgresql.Driver");
+                                Connection con = DriverManager.getConnection ("jdbc:postgresql://localhost:5432/dama","postgres","ISA681DamaP!");
+		    		String query = "INSERT INTO USERS VALUES (?,?,?)";
+
 		    		PreparedStatement ps = con.prepareStatement(query);
 		    		ps.clearParameters();
-		    		ps.setString(1, email);
-		    		ps.setString(2, username);
+		    		ps.setString(2, email);
+		    		ps.setString(1, username);
 		    		ps.setString(3, password1);
 		    		ps.executeUpdate();
+
+                                response.sendRedirect("./success.html");
+                                con.close();
+                                
 	    		} 
 	        catch (SQLException | ClassNotFoundException e) {
 	        		e.printStackTrace();
@@ -62,6 +86,6 @@ public class NewUser extends HttpServlet {
         else {
         		response.sendRedirect("./failure.html");
         }
-        response.sendRedirect("./success.html");
+ //       response.sendRedirect("./success.html");
 	}
 }
