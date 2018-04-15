927cf45e0452c030d26e8f46e020e9689bdfff35
chabib19 <37488737+chabib19@users.noreply.github.com>
2018-04-15 14:41:31 -0400

    Update NewUser.java

--- dama/src/NewUser.java
+++ dama/src/NewUser.java
@@ -1,11 +1,20 @@
-
+//TODO list added by Chadia
+//done// should present user-friendly message when password1 and password2 are not equal
+//done// should present user-friendly message when username already exist in DB (should not wait for SQL exception)
+//done// should add logging entry for user created
+//done// should remove hard-coded DB credentials from this file and from Validate.java. Instead read these creds from a server config file.
+// should add input validation (whitelist w/ regular expressions) for the email
+//done// should not be storing passwords in clear form. Instead store only salted hash.
+// should encrypt connections to the DB (use TLS encryption)
+// question: should one email be allowed to register multiple usernames?
+// consider: validating the exitance of the email (ie. send confirmation link to prove ownership of the email being registered)
+// consider enforcing a password length and complexity for the user's password
+// consider adding captcha for preventing bots from abusing this form (least important task for now)
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.security.SecureRandom;
 import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
@@ -14,7 +23,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.errors.IntrusionException;
+import org.owasp.esapi.errors.ValidationException;
 
 /**
  * Servlet implementation class NewUser
@@ -22,7 +34,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/NewUser")
 public class NewUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    //static Logger log = Logger.getLogger(NewUser.class);
+        static Logger log = Logger.getLogger(NewUser.class);
+        static Connection con = null;
+        static int password_max_length = 16;
+        static int email_max_length = 42;
+        static int username_max_length = 20;
+
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -33,8 +50,13 @@ public class NewUser extends HttpServlet {
     }
 
 	/**
+     * @param request
+     * @param response
+     * @throws javax.servlet.ServletException
+     * @throws java.io.IOException
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+        @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
                 
@@ -44,69 +66,65 @@ public class NewUser extends HttpServlet {
         String email = request.getParameter("email");
         String username = request.getParameter("username");
         String password1 = request.getParameter("password1");
-        String password2 = request.getParameter("password2");
-        if (password1.equals(password2)) {
-//TODO list added by Chadia
-// should present user-friendly message when password1 and password2 are not equal
-//done// should present user-friendly message when username already exist in DB (should not wait for SQL exception)
-// should add logging entry for user created
-//done// should remove hard-coded DB credentials from this file and from Validate.java. Instead read these creds from a server config file.
-// should add input validation (whitelist w/ regular expressions) for the email
-//done// should not be storing passwords in clear form. Instead store only salted hash.
-// should encrypt connections to the DB (use TLS encryption)
-// question: should one email be allowed to register multiple usernames?
-// consider: validating the exitance of the email (ie. send confirmation link to prove ownership of the email being registered)
-// consider enforcing a password length and complexity for the user's password
-// consider adding captcha for preventing bots from abusing this form (least important task for now)
-	        try {
-
-/*		        String email = request.getParameter("email");
-		        String username = request.getParameter("username");
-		        String password1 = request.getParameter("password1");
-		        String password2 = request.getParameter("password2");
-*/		
-
-/*
-		        	Class.forName("oracle.jdbc.driver.OracleDriver");
-		    		Connection con = DriverManager.getConnection ("jdbc:oracle:thin:@dama.cc0rojk8d4jm.us-east-1.rds.amazonaws.com:1521:dama","swe681","SWEpass123");
-		    		String query = "INSERT INTO USERS VALUES (?,?,?)";
-*/
-
-		GetPropertyValues properties = new GetPropertyValues();
-		String dburl = properties.getPropValues("dburl");
-                String dbuser = properties.getPropValues("dbuser");
-                String dbpassword = properties.getPropValues("dbpassword");
-
-                        Class.forName("org.postgresql.Driver");
-                        Connection con = DriverManager.getConnection (dburl,dbuser,dbpassword);
+        String confirmationPassword = request.getParameter("password2");
  
+        
+
+	        try {
+                    
+                            String cleaned_email = ESAPI.validator().getValidInput("SignupPage_EmailField",email,
+                                  "Email", // regex spec
+                                  email_max_length, // max lengyh
+                                  false, // no nulls
+                                  true); // canonicalize
+        
+                            String cleaned_username = ESAPI.validator().getValidInput("SignupPage_UsernameField",username,
+                                  "Username", // regex spec
+                                  username_max_length, // max lengyh
+                                  false, // no nulls
+                                  true); // canonicalize
+                            
+                            String cleaned_password1 = ESAPI.validator().getValidInput("SignupPage_PasswordField",password1,
+                                  "ComplexPassword", // regex spec
+                                  password_max_length, // max lengyh
+                                  false, // no nulls
+                                                                      true); // canonicalize
+
+
+                    
+                    if (cleaned_password1.equals(confirmationPassword)) {
+
+
+               
+                        con = ConnectionManager.getConnection();
 /*                                SecureRandom salt = new SecureRandom();
                                 byte bytes[] = new byte[20];
                                 salt.nextBytes(bytes);
 */
 
-                        String computed_hash = PasswordUtil.hashPassword(password1);
+                        String computed_hash = PasswordUtil.hashPassword(cleaned_password1);
                                 
-		    	String checkUser = "SELECT username from damauser where username=?";
+		    	String checkUser = "SELECT username from dama_user where username=?";
                         PreparedStatement ck = con.prepareStatement(checkUser);
                         ck.clearParameters();
-                        ck.setString(1, username);
+                        ck.setString(1, cleaned_username);
+                         log.debug("Executing query to check if user already exist"); 
                         ResultSet foundUser = ck.executeQuery();
                         boolean exists = foundUser.next();
                         
                         if(!exists){
 
                                 
-		    		String query = "INSERT INTO damauser VALUES (?,?,?)";
+		    		String query = "INSERT INTO dama_user VALUES (?,?,?)";
 
 		    		PreparedStatement ps = con.prepareStatement(query);
 		    		ps.clearParameters();
-		    		ps.setString(2, email);
-		    		ps.setString(1, username);
+                                ps.setString(1, cleaned_username);
+		    		ps.setString(2, cleaned_email);
 		    		ps.setString(3, computed_hash);
 		    		ps.executeUpdate();
 
-                                //log.debug("Created new User");
+                                log.debug("Created new User");
                                 con.close();
                                 response.sendRedirect("./success.html");
                         }
@@ -116,19 +134,44 @@ public class NewUser extends HttpServlet {
                             
                         }
                         
-                        
+             }
+        else {
+        		response.sendRedirect("./ConfirmPassword.html");
+        }           
                                 
 	    		} 
 
-	        catch (Exception e) {
-	        		e.printStackTrace();
+	        catch (SQLException e) {
+                                log.error("Encounter SQL Exception in NewUser : " + e );
                                 response.sendRedirect("./failure.html");
 	        }
+                catch (IntrusionException e) {
+                                log.error("Encounter Intrusion Attempt in NewUser : " + e );
+                                //if valid user, then lock the user's account
+                                
+                                try {
+                                    String valid_username = ESAPI.validator().getValidInput("SignupPage_UsernameField",username,
+                                  "Username", // regex spec
+                                  username_max_length, // max lengyh
+                                  false, // no nulls
+                                  true); // canonicalize
+                                    
+                                    String locked_user = ESAPI.encoder().encodeForXML(valid_username);
+                                    
+ //todo//                                   // lock the user account
+                                log.error("Deactivating user account due to Intrusion Exception : " + locked_user );
+                        
+                                } catch (IntrusionException | ValidationException ex) {
+                                    //no need for further action
+                                }
+                                
+                                response.sendRedirect("./failure.html");
+	        }
+                catch (ValidationException e){
+                    log.debug("Validation Error in NewUser" + e);
+                    response.sendRedirect("./incorrectInput.html");
+                }
 
-        }
-        else {
-        		response.sendRedirect("./ConfirmPassword.html");
-        }
- //       response.sendRedirect("./success.html");
+        
 	}
 }
