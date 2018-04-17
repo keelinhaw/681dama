66e9ca1b2f12a1e237bf8f586cbaf644e08c6ca0
chabib19 <chabib@masonlive.gmu.edu>
2018-04-17 01:08:41 -0400

    added ESAPI authenticator

--- dama/src/Login.java
+++ dama/src/Login.java
@@ -3,6 +3,7 @@
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +12,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.Randomizer;
+import org.owasp.esapi.User;
+import org.owasp.esapi.errors.AccessControlException;
+import org.owasp.esapi.errors.AuthenticationException;
+import org.owasp.esapi.errors.ValidationException;
 
 /**
  * Servlet implementation class Login
@@ -20,6 +27,8 @@ public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
         static Logger log = Logger.getLogger(Login.class);
         static Connection con = null;
+        static int password_max_length = 16;
+        static int username_max_length = 20;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -44,21 +53,72 @@ public class Login extends HttpServlet {
 		// TODO Auto-generated method stub
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        
+
+        
+
+
+//        RequestDispatcher dispatcher;
+
         try {
-			if(Validate.checkUser(username, password)) {
-		        HttpSession session = request.getSession();  
+            
+//todo//                            ESAPI.httpUtilities().assertSecureRequest(request);
+
+
+                        String cleaned_username = ESAPI.validator().getValidInput("LoginPage_UsernameField",username,
+                                  "Username", // regex spec
+                                  username_max_length, // max lengyh
+                                  false, // no nulls
+                                  true); // canonicalize
+ //todo//                           
+ /*                           String cleaned_password = ESAPI.validator().getValidInput("LoginPage_PasswordField",password,
+                                  "ComplexPassword", // regex spec
+                                  password_max_length, // max lengyh
+                                  false, // no nulls
+                                                                      true); // canonicalize            
+ */           
+ 
+//        HttpSession session = request.getSession(true); 
+        ESAPI.httpUtilities().setCurrentHTTP(request, response);
+ /*                       User user = ESAPI.authenticator().login(request, response);
+                        user.addSession(session);
+*/                        
+			if(Validate.checkUser(cleaned_username, password)) {
+
+
+                        HttpSession session = ESAPI.httpUtilities().changeSessionIdentifier(request);
+//                        HttpSession session = request.getSession(true);
 		        session.setAttribute("username", username);
+                        
                         //Opponent player = new Opponent();
 		        //player.newOpponent(username);
                         
-				response.sendRedirect("./landing.jsp");
+//				response.sendRedirect("/681dama/landing.jsp");
+                                ESAPI.httpUtilities().sendRedirect(response, "/681dama/landing.jsp");
+//                                ESAPI.httpUtilities().sendForward(request, response, "WEB-INF/landing.jsp");
+//                                dispatcher = request.getRequestDispatcher("./landing.jsp");
+//				dispatcher.forward(request, response);
 			}
 			else {
 				response.sendRedirect("./failure.html");
+//				dispatcher = request.getRequestDispatcher("WEB-INF/failure.html");
+//				dispatcher.forward(request, response);
 			}
 		} catch (ClassNotFoundException | SQLException e) {
                     log.debug("Login Error" + e);
 		}
+                catch (ValidationException | AuthenticationException e){
+                    log.debug("Login Error " + e);
+                    response.sendRedirect("./failure.html");
+ //                   dispatcher = request.getRequestDispatcher("WEB-INF/failure.html");
+ //                   dispatcher.forward(request, response);
+                }
+                catch ( AccessControlException e ){
+                    log.debug("Login Error " + e);
+                    response.sendRedirect("./failure.html");
+                }
+
+        
 		doGet(request, response);
 	}
 
