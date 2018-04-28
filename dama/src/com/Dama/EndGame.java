4f86dce2d82176ffbce925d0d952a1fe7dfd262b
chabib19 <chabib@masonlive.gmu.edu>
2018-04-28 17:32:06 -0400

    older updates that were made to GameHistory before it moved to com.Dama

--- dama/src/com/Dama/EndGame.java
+++ dama/src/com/Dama/EndGame.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.server.GameServer;
-
 /**
  * Servlet implementation class EndGame
  */
